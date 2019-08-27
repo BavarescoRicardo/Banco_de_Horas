8d87072b564354f73b33a8c0f173871cd49aeecd
ricardo <ricardo.bav17@gmail.com>
2019-08-27 15:39:22 -0300

    mudancas no banco sql

---
+++ bdHoralSql3.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bancohoras
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `extra`
+--
+
+DROP TABLE IF EXISTS `extra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `extra` (
+  `idExtra` int(11) NOT NULL AUTO_INCREMENT,
+  `quantidadeH` int(11) DEFAULT NULL,
+  `quantidadeMin` int(11) DEFAULT NULL,
+  `dia` datetime DEFAULT NULL,
+  `observacao` varchar(255) DEFAULT NULL,
+  `funcionarioFk` int(11) NOT NULL,
+  PRIMARY KEY (`idExtra`),
+  KEY `funcionarioFk` (`funcionarioFk`),
+  CONSTRAINT `extra_ibfk_1` FOREIGN KEY (`funcionarioFk`) REFERENCES `funcionario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `extra`
+--
+
+LOCK TABLES `extra` WRITE;
+/*!40000 ALTER TABLE `extra` DISABLE KEYS */;
+INSERT INTO `extra` VALUES (1,2,0,'2019-08-20 00:00:00','Formatando computadores',4826),(2,5,20,'2018-10-10 00:00:00','testanto sistema',1),(3,200,1,'2019-08-25 16:23:24','denovo testando',1),(4,10,50,'2019-08-30 16:25:15','novamente testando',4826),(5,2,55,'2019-08-05 00:48:06','Denovo testando',4826);
+/*!40000 ALTER TABLE `extra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `funcionario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `cargo` varchar(255) DEFAULT NULL,
+  `ativo` int(11) DEFAULT NULL,
+  `setorfk` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `setorfk` (`setorfk`),
+  CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`setorfk`) REFERENCES `setores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` VALUES (1,'Teste','Cobaia',1,2),(1236,'Jubileu','Pipoqueiro',0,1),(4578,'Teste22','Experimentor',1,3),(4826,'Ricardo Bavaresco','Tecnico em Informatica',1,1);
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) DEFAULT NULL,
+  `senha` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `setores`
+--
+
+DROP TABLE IF EXISTS `setores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `setores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `setores`
+--
+
+LOCK TABLES `setores` WRITE;
+/*!40000 ALTER TABLE `setores` DISABLE KEYS */;
+INSERT INTO `setores` VALUES (1,'Administracao'),(2,' Saude'),(3,' Social');
+/*!40000 ALTER TABLE `setores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-27 15:38:22
